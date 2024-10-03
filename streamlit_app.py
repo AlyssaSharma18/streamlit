7152c6e24a11427be3bb6d40cdd59d0947cb2523
AlyssaSharma18 <91567337+AlyssaSharma18@users.noreply.github.com>
2024-10-03 18:52:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,8 +66,13 @@ for i in options:
 
 for i in range(len(options)):
     st.write(i)
+
+for i in range(len(options)):
+    exec(f'col{i} = "Column {i}"')
     
-col1, col2, col3 = st.columns(3)
+st.columns(len(options)) 
+# col1, col2, col3 = st.columns(3)
+
 col1.metric("Temperature", "70 °F", "1.2 °F")
 col2.metric("Wind", "9 mph", "-8%")
 col3.metric("Humidity", "86%", "4%")
