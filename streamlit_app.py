4e09426bab8af3160ca1911c6daf4d8fb5ecdac5
AlyssaSharma18 <91567337+AlyssaSharma18@users.noreply.github.com>
2024-10-03 18:42:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,12 @@ else:
     st.write("No sales data available for the selected subcategory.")
 
 
+# Question 4
 
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
 
 #Return to original code
 
